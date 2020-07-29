c39512db7fc970c24878dc565c27e78ccdd8d5e2
selplacei <35859522+selplacei@users.noreply.github.com>
2020-07-28 22:11:17 -0700

    Initial commit

---
+++ ViewportSizeBox.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        lbs-viewport-size-box
+// @namespace   Violentmonkey Scripts
+// @match       https://littlebigsnake.com/*
+// @grant       none
+// @version     1.0
+// @author      selplacei
+// @description Adds a small box in the bottom right to see the size of the viewport.
+// @run-at      document-end
+// @noframes
+// @downloadURL https://raw.githubusercontent.com/selplacei/lbs-viewport-size-box/master/ViewportSizeBox.js
+// @homepageURL https://github.com/selplacei/lbs-viewport-size-box
+// ==/UserScript==
+function updatewhview(_) {
+  document.getElementById("whview_width").innerHTML = window.innerWidth;
+  document.getElementById("whview_height").innerHTML = window.innerHeight;
+}
+document.getElementsByTagName("BODY")[0].onload = () => {
+  window.onresize = updatewhview;
+  var whview = document.createElement("DIV");
+  whview.style = "background: #000; color: #fff; position: absolute; right: 0px; bottom: 0px; font-family: monospace;"
+  whview.innerHTML = "<span id=\"whview_width\"></span>x<span id=\"whview_height\"></span>";
+  document.getElementsByTagName("BODY")[0].appendChild(whview);
+  updatewhview(null);
+}
+
